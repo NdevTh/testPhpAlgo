8a2d5057e4596149334995f5c41d4e606f231dcf
ct mac <cth.huynh@gmail.com>
2023-11-07 13:50:06 +0100

    phpLesson

---
+++ phpLess.php
@@ -0,0 +1,39 @@
+<?php
+
+
+// ------>>>>année bissextile
+// echo "Veuillez entrer une année : ";
+// $date = trim(fgets(STDIN));
+
+// if (($date % 4 == 0 && $date % 100 != 0) || ($date % 400 == 0)) {
+//     echo "Votre année est bissextile";
+// } else {
+//     echo "Année normale";
+// }
+// ---->>> l'ordre des nom / mot <<<<------
+// echo " veuillez saisir le premier nom1 :";
+// $nam1 = trim(fgets(STDIN));
+// echo " veuillez saisir le premier nom2 : ";
+// $nam2 = trim(fgets(STDIN));
+// echo " veuillez saisir le premier nom3 : ";
+// $nam3 = trim(fgets(STDIN));
+
+// if ($nam1<$nam2 && $nam2<$nam3 ) {
+//     echo "les noms sont dans l'ordre alphabet";
+//     }
+//     elseif ($nam1>$nam2 && $nam2>$nam3) {
+//         echo" les noms sont dans l'ordre décroissant";
+//         # code...
+//     } else {
+//         echo"c'est désordre";
+//     }
+
+
+//-->>>>>>>>> count  words and caracter
+$mots = 0;
+echo "veuillez entrée une phrase"
+
+
+
+?>
+
