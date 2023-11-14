6ab363a5afc747855c55b1332a3712522c3998e2
ct mac <cth.huynh@gmail.com>
2023-11-14 13:34:23 +0100

    phpRemboursementExercice

---
+++ exercicePhp.php
@@ -0,0 +1,21 @@
+<?php
+// remboursement 
+// $mont= 2500;
+// $mois = 0;
+
+// while ($mont > 110) {
+
+//     $mont = $mont - 110;
+//     $mois++ ;
+
+    
+// }
+// echo " le nombre de mois rembourse est " . $mois ." mois\n";
+// echo " il reste " . $mont ."euros";
+
+
+
+?> 
+<?php
+$haut
+?>
